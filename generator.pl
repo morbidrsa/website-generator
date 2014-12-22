c42d4febe490d5bb4b444b354e8e7f4e75a4c0e8
Johannes Thumshirn <morbidrsa@gmail.com>
2014-12-22 21:19:33 +0100

    Initial import
    Signed-off-by: Johannes Thumshirn <morbidrsa@gmail.com>

---
+++ generator.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use Getopt::Long;
+
+sub process_file {
+    my $layout_html = shift;
+    my $content_dir = shift;
+    my $output_dir = shift;
+    my $content_file = shift;
+
+    my $layout;
+    my $content;
+    my $outfile;
+
+    open($layout, $layout_html) or die("Could not open $layout_html for reading: $!");
+    open($content, $content_dir . $content_file) or die("Could not open $content_file for reading: $!");
+    open($outfile, '>', $output_dir . $content_file) or die("Could not open $outfile for writing: $!");
+
+    while (<$layout>) {
+	my $content_tag;
+
+	$content_tag = $1 if /\[%\s?(CONTENT)?\s%\]/;
+	if ($content_tag) {
+	    while (<$content>) {
+		print $outfile $_;
+	    }
+	} else {
+	    print $outfile $_;
+	}
+    }
+}
+
+sub process_content {
+    my $content_dir = shift;
+    my $output_dir = shift;
+    my $layout_html = shift;
+
+    my $content_file;
+    my @content_files;
+
+    opendir(my $content, "$content_dir") or die("Can't open content directory $content_dir: $!");
+    @content_files = readdir($content);
+    closedir($content);
+
+
+    foreach $content_file (@content_files) {
+	next if $content_file eq ".." || $content_file eq ".";
+
+	print "\$content_file = $content_file\n";
+	process_file($layout_html, $content_dir, $output_dir, $content_file);
+	print "\n\n";
+    }
+}
+
+my $layout_html = '';
+my $content_dir = '';
+my $output_dir = '';
+
+GetOptions('layout-html=s' => \$layout_html,
+	   'content-dir=s' => \$content_dir,
+	   'output-dir=s' => \$output_dir);
+
+die("Need a layout") if $layout_html eq '';
+die("Need a content directory") if $content_dir eq '';
+die("Need an output directory") if $output_dir eq '';
+
+process_content($content_dir, $output_dir, $layout_html);
