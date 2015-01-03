592e68be0fcc47b6233364f3f4dbc40104360a2f
Johannes Thumshirn <morbidrsa@gmail.com>
2015-01-03 13:35:01 +0100

    generator.pl: Add processing of sitemaps
    Add processing of sitemaps to create 1 dimensional menu structures.
    
    Signed-off-by: Johannes Thumshirn <morbidrsa@gmail.com>

--- generator.pl
+++ generator.pl
@@ -10,6 +10,7 @@ sub process_file {
     my $content_dir = shift;
     my $output_dir = shift;
     my $content_file = shift;
+    my $navigation = shift;
 
     my $layout;
     my $content;
@@ -21,9 +22,14 @@ sub process_file {
 
     while (<$layout>) {
 	my $content_tag;
+	my $navigation_tag;
 
+	$navigation_tag = $1 if /\[%\s?(NAVIGATION)?\s%\]/;
 	$content_tag = $1 if /\[%\s?(CONTENT)?\s%\]/;
-	if ($content_tag) {
+
+	if ($navigation_tag) {
+	    print $outfile $navigation;
+	} elsif ($content_tag) {
 	    while (<$content>) {
 		print $outfile $_;
 	    }
@@ -40,7 +46,10 @@ sub process_content {
     my $content_dir = $options{content_dir};
     my $output_dir = $options{output_dir};
     my $layout_html = $options{layout_html};
+    my $sitemap_file = $options{sitemap};
+    my $navigation;
     my $content_file;
+    my $sitemap;
     my @content_files;
 
     opendir(my $content, "$content_dir") or die("Can't open content directory $content_dir: $!");
@@ -48,19 +57,38 @@ sub process_content {
     closedir($content);
 
 
+    open($sitemap, $sitemap_file) or die("Can't open sitemap file $sitemap_file: $!");
+    $navigation = "<ul>\n";
+    while (<$sitemap>) {
+	my $file;
+	my $text;
+
+	($file, $text) = split(/=/, $_);
+
+	chomp($file);
+	chomp($text);
+
+	$navigation .= "\t<li><a href=\"$file\">$text</a></li>\n";
+    }
+    $navigation .= "</ul>\n";
+    close($sitemap);
+
+    print $navigation;
+
     foreach $content_file (@content_files) {
 	next if $content_file eq ".." || $content_file eq ".";
 
 	print "Processing $content_file\n";
-	process_file($layout_html, $content_dir, $output_dir, $content_file);
+	process_file($layout_html, $content_dir, $output_dir, $content_file, $navigation);
     }
 }
 
 sub usage {
-    print "$0 <layout> <content> <output>\n";
+    print "$0 <layout> <content> <output> <sitemap>\n";
     print "\t--layout-html <layout>\n";
     print "\t--content-dir <content>\n";
     print "\t--output-dir <output>\n";
+    print "\t--sitemap <sitemap>\n";
 
     exit(1);
 }
@@ -68,18 +96,22 @@ sub usage {
 my $layout_html = '';
 my $content_dir = '';
 my $output_dir = '';
+my $sitemap = '';
 my %options;
 
 GetOptions('layout-html=s' => \$layout_html,
 	   'content-dir=s' => \$content_dir,
-	   'output-dir=s' => \$output_dir);
+	   'output-dir=s' => \$output_dir,
+	   'sitemap=s' => \$sitemap);
 
 usage() if $layout_html eq '';
 usage() if $content_dir eq '';
 usage() if $output_dir eq '';
+usage() if $sitemap eq '';
 
 $options{'layout_html'} = $layout_html;
 $options{'output_dir'} = $output_dir;
 $options{'content_dir'} = $content_dir;
+$options{'sitemap'} = $sitemap;
 
 process_content(%options);
